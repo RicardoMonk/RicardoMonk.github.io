3563770f9159eec126c155f56d12d58c822baa63
Ricardo Monk <ricardodgcvisual@gmail.com>
2021-04-24 11:51:12 -0500

    seccion 4 blog

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*Efecto para menu*/
 var burgerMenu = document.getElementById('burger-menu');
 var show = document.getElementById('menu');
 
@@ -5,3 +6,19 @@ burgerMenu.addEventListener('click', function(){
     this.classList.toggle("close");
     show.classList.toggle("show");
 })
+
+/*Efecto de textos animados*/
+let animux = document.querySelectorAll(".animux");
+
+function mostrarscroll(){
+    let scrollTop = document.documentElement.scrollTop;
+    for (var i=0; i< animux.length; i++ ) {
+        let alturaanimux = animux[i].offsetTop;
+        if (alturaanimux - 400 < scrollTop) {
+            animux[i].style.opacity =1;
+            animux[i].classList.add("mostrarArriba");
+        }
+    }
+}
+
+window.addEventListener('scroll', mostrarscroll);
